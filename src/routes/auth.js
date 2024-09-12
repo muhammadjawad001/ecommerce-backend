868da23ca6e95ae2f150eacdd20b2402d069f57f
muhammadjawad001 <jawadmobeen01@gmail.com>
2024-09-12 18:33:37 +0500

    Initialized Swagger

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,6 +2,20 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
+
+/**
+ * @swagger
+ * /sample:
+ *   get:
+ *     summary: Returns a sample message
+ *     responses:
+ *       200:
+ *         description: A successful response
+ */
+router.get('/', (req, res) => {
+    res.json({ message: 'This is a sample message' });
+  });
+
 // Signup route
 router.post('/signup', authController.signup);
 
